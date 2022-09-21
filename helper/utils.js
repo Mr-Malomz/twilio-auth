eac664ed74897185a022a34161e1ea804403bdc9
demola malomo <malomo.alaba@yahoo.com>
2022-09-21 01:17:03 +0100

    sms test

--- helper/utils.js
+++ helper/utils.js
@@ -2,15 +2,14 @@ import { Client, Account } from 'appwrite';
 
 //create client
 const client = new Client();
-client.setEndpoint('http://localhost/v1').setProject('63276bcf4bd7f83f71c7');
+client.setEndpoint('http://127.0.0.1/v1').setProject('632a4573175fcb99d621');
 
 //create account
 const account = new Account(client);
 
 //authenticate user with phone number
 export const phoneAuth = (phone_number) => {
-  return account.createPhoneSession('unique()', phone_number);
-  // return account.createAnonymousSession();
+  return account.createPhoneSession('632a4c40f3e29cfb7834', phone_number);
 };
 
 //validate code
